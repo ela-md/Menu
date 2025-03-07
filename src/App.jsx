dad31dee6304860dd6407b2c2548b745fbbd05eb
ela.md <ela.ahmadi06@gmail.com>
2025-03-07 13:27:35 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Title from "./Title";
+import Menu from "./Menu";
+
+
+function App  ()  {
+
+
+  return (
+
+  <>
+  <h2>our menu</h2>
+  </>
+
+
+
+  )
+};
+export default App;
