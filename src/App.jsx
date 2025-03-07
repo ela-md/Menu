5439e26295142aa4f04e81c2caaccb7916ef9498
ela.md <ela.ahmadi06@gmail.com>
2025-03-07 13:50:02 -0500

    data rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 
 import Title from "./Title";
+import Menu from "./Menu";
 import AppProvider from "./context";
 
 function App  ()  {
 
-
   return (
 
-   
 <AppProvider>
-
     <main>
-      <section className="menu">   
+      <section className="menu"> 
         <Title  />
+        <Menu  />
       </section>
     </main>
-
 </AppProvider>
-
-
-
-
   )
 };
 export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,21 @@
+
+import { AppContext } from './context'
+import { useContext } from 'react'
+import MenuItem from './MenuItem'
+
+function Menu () {
+
+    const{menuItems} = useContext(AppContext);
+
+    return(
+      <div className="section-center">
+       {menuItems.map((menuItem) => {
+        
+         return  <MenuItem key={menuItem.id} {...menuItem} />
+        
+       })}
+      </div>
+    )
+}
+
+export default Menu
